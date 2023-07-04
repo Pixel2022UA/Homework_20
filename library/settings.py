e39dd4611cb1759660fd0df6c49839a8f96900b4
Pixel2022UA <zelenskiy2013@gmail.com>
2023-07-04 18:13:53 +0300

    fix3

--- library/settings.py
+++ library/settings.py
@@ -134,12 +134,3 @@ STORAGES = {
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
